20181ccf857b06fedc13dc40c3daf4f877f8eb03
blopa <pabloosabaterr@gmail.com>
2021-01-18 19:23:23 +0100

    refactor

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,28 +5,33 @@ import com.company.controllers.MainController;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
-
 
     public static void main(String[] args) throws IOException {
         MainController controller = new MainController();
         String commandLine;
         BufferedReader console = new BufferedReader
                 (new InputStreamReader(System.in));
-        controller.newAsnwer("Welcome to our shell https://github.com/Blopaa/BlyShell");
+        SimpleDateFormat format = new SimpleDateFormat("(yyyy/MM/dd HH:mm)");
+        String dateString = format.format(new Date());
+        System.out.println("Welcome to BLY version v" + controller.version);
+        System.out.println(dateString + " https://github.com/Blopaa/BlyShell");
 
         while (true) {
-            controller.newEntrie();
+            System.out.print("> ");
             commandLine = console.readLine();
             switch (commandLine) {
                 case "exit":
                     System.exit(0);
+                    break;
                 default:
                     if (controller.evaluate(commandLine) != null) {
-                        controller.newAsnwer(controller.evaluate(commandLine));
+                        System.out.println(controller.evaluate(commandLine));
                     } else {
-                        controller.newAsnwer("error, command not found");
+                        System.out.println("error, command not found");
                     }
             }
         }
--- src/com/company/controllers/MainController.java
+++ src/com/company/controllers/MainController.java
@@ -2,20 +2,10 @@ package com.company.controllers;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MainController {
 
-    public void newEntrie() {
-        SimpleDateFormat format = new SimpleDateFormat("(yyyy/MM/dd HH:mm)");
-        String dateString = format.format(new Date());
-        System.out.print("BLY " + dateString + " > ");
-    }
-
-    public void newAsnwer(String msg){
-        System.out.println("> " + msg);
-    }
+    public static final String version = "0.0.1";
 
     public String evaluate(String expression) {
         try {
